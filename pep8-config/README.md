a4a7c76789b5d9cc0b8ea1544f56125862be4324
zshapiro <zack@textur.com>
2015-09-02 15:42:21 -0700

    Adding brief script to setup pep8 conventions.

---
+++ pep8-config/README.md
@@ -0,0 +1,5 @@
+Pep8 Conventions
+===================
+The pep8 file defines our standard exceptions.  To use it clone this repo and run `bootstrap.sh`. 
+
+Exceptions are explained at [pep8.readthedocs.org](http://pep8.readthedocs.org/en/latest/intro.html#error-codes).
---
+++ pep8-config/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+if ! [ "$DIR" ]; then
+  mkdir ~/.config 
+fi
+pushd ~/.config
+if [ -L "./pep8" ]; then
+  TIMESTAMP=`date +"%s"`
+  echo "Moving pep8 to pep8.$TIMESTAMP"
+  mv pep8 pep8.$TIMESTAMP  
+fi
+ln -s $DIR/pep8 pep8
+popd
