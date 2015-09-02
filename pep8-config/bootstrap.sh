6188eb431c3167a5fb55235453e882ff990369fd
zshapiro <zack@textur.com>
2015-09-02 15:49:58 -0700

    Check for actual file as well as symlink.

--- pep8-config/bootstrap.sh
+++ pep8-config/bootstrap.sh
@@ -4,10 +4,10 @@ if ! [ "$DIR" ]; then
   mkdir ~/.config 
 fi
 pushd ~/.config
-if [ -L "./pep8" ]; then
+if [ -L "./pep8" ] || [ -e "./pep8" ]; then
   TIMESTAMP=`date +"%s"`
-  echo "Moving pep8 to pep8.$TIMESTAMP"
-  mv pep8 pep8.$TIMESTAMP  
+  echo "Moving pep8 to pep8.${TIMESTAMP}"
+  mv pep8 pep8.${TIMESTAMP}
 fi
-ln -s $DIR/pep8 pep8
+ln -s ${DIR}/pep8 pep8
 popd
